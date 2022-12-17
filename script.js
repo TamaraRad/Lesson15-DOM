9966726c63126a610223e9996132f7f17a57beb5
TamaraRad <98638833+TamaraRad@users.noreply.github.com>
2022-12-17 15:49:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const addBtn = document.querySelector('#addBtn');
+const inputName = document.querySelector('#inputName');
+const list = document.querySelector('#list');
+
+
+//newItem.classList.add('item');//
+
+addBtn.addEventListener('click', function() {
+    const newItem = document.createElement('li');
+    newItem.classList.add('item');
+    newItem.textContent = inputName.value;
+    list.append(newItem);
+
+    const delBtn = document.createElement('button');
+    delBtn.textContent = 'Delete';
+    newItem.append(delBtn);
+
+    delBtn.addEventListener('click', function () {
+        newItem.remove()
+    })
+    inputName.value = '';
+});
